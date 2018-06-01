d8db566dcdc5f94314c04304bc3290acbae33bc5
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-06-01 11:48:42 -0300

    Implemented form validation

--- casadocodigo/app/routes/product.js
+++ casadocodigo/app/routes/product.js
@@ -19,13 +19,29 @@ module.exports = (app) => {
     });
 
     app.get('/product', (req, res) => {
-        res.render('product/form')
+        res.render('product/form', {errors: [], product: {}})
     });
 
     app.post('/product', (req, res) => {
 
+        req.assert('titulo', 'Title is required').notEmpty();
+        req.assert('preco', 'Price is required').isFloat();
+        var errors = req.validationErrors();
+        
         var product = req.body;
+
         console.log(product);
+        if (errors) {
+            res.format({
+                html: () => {
+                    res.status(400).render('product/form', {errors: errors, product: product});
+                },
+                json: () => {
+                    res.status(400).json(errors);
+                }
+            });
+            return;
+        }
 
         var conn = app.infra.databaseFactory();
         var productDAO = new app.infra.ProductDAO(conn);
