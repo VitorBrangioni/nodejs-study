d3335401b9139f7db6adfe6b296335f6519c888a
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-06-01 13:47:06 -0300

    Test using nodejs default library

--- casadocodigo/test/productTest.js
+++ casadocodigo/test/productTest.js
@@ -1,6 +1,35 @@
+var http = require('http');
+var assert = require('assert');
+
 describe('#ProductController', () => {
-    it('#list json', () => {
-        console.log('listing products in json format');
+    it('#list json', (done) => {
+
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/product',
+                method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            }
+        };
+
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/products',
+            headers: {
+                'Accept': 'application/json'
+                //'Accept': 'text/html'
+            }
+        };
+        
+        http.get(configuracoes, function(res){
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
+            done();
+        });
     });
 
 });
